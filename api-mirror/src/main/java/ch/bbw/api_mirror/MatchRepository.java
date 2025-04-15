5a2dd53d2cebcd4b7643ba270d5b10af3c2a0ebd
Andrin <andrinrenggli@outlook.com>
2025-04-15 09:14:56 +0200

    repo

--- api-mirror/src/main/java/ch/bbw/api_mirror/MatchRepository.java
+++ api-mirror/src/main/java/ch/bbw/api_mirror/MatchRepository.java
@@ -1,4 +1,9 @@
 package ch.bbw.api_mirror;
 
-public class MatchRepository {
+import ch.bbw.api_mirror.model.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface MatchRepository extends JpaRepository<Match, Long> {
+    List<Match> findByCompetitionAndStatus(String competition, String status);
 }
