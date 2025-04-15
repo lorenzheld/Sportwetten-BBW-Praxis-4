04febd0b119542fccbcea47d79421c7b9b6144ed
Andrin <andrinrenggli@outlook.com>
2025-04-15 09:12:24 +0200

    add match class

--- api-mirror/src/main/java/ch/bbw/api_mirror/model/Match.java
+++ api-mirror/src/main/java/ch/bbw/api_mirror/model/Match.java
@@ -1,4 +1,26 @@
 package ch.bbw.api_mirror.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Match {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+    private String homeTeam;
+    private String awayTeam;
+    private String competition;
+    private String season;
+    private String date;
+
+
 }
