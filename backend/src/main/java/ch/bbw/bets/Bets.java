0bcd5f8ef91c3354e5d7c58c072feac6889e8a31
Andrin <andrinrenggli@outlook.com>
2025-06-17 09:29:43 +0200

    added bets

--- backend/src/main/java/ch/bbw/bets/Bets.java
+++ backend/src/main/java/ch/bbw/bets/Bets.java
@@ -1,4 +1,29 @@
 package ch.bbw.bets;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="bets")
 public class Bets {
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    @Id
+    private UUID id;
+    long gameId;
+    UUID userId;
+
+    public Bets(UUID id, long gameId, UUID userId) {
+        this.id = id;
+        this.gameId = gameId;
+        this.userId = userId;
+    }
 }
